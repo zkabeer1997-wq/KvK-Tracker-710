df2498a4820cc750e958e6ed862892870207cf81
zkabeer1997-wq <zkabeer1997@gmail.com>
2026-08-03 17:10:55 -0400

    Create SiteHeader.js

---
+++ components/SiteHeader.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export default function SiteHeader() {
+  return (
+    <header className="site-header">
+    <div className="site-header-inner">
+    <Link href="/" className="site-brand">K710</Link>
+  <nav className="site-nav" aria-label="Main site">
+    <Link href="/">Home</Link>
+  <Link href="/player-record">Rallies</Link>
+  <Link href="/interest">Interest</Link>
+  <Link href="/power-profile">Power Profile</Link>
+  <Link href="/admin">Admin</Link>
+    </nav>
+    </div>
+    </header>
+  );
+}
