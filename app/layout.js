f50b00d058264316587daaef85fa85a729c84542
zkabeer1997-wq <zkabeer1997@gmail.com>
2026-08-03 14:37:44 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'K710 KvK Planner',
-  description: 'KvK Tracker 710 troop, hero and availability submission form',
+ title: 'K710 KvK Planner',
+ description: 'KvK Tracker 710 troop, hero and availability submission form',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-    <body>{children}</body>
-    </html>
-  );
+ return (
+ <html lang="en">
+ <body>{children}</body>
+ </html>
+ );
 }
