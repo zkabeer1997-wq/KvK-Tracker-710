e4c9e0235e08025fb3dcade8cb86992d9301a625
zkabeer1997-wq <zkabeer1997@gmail.com>
2026-08-06 08:45:53 -0400

    Update SiteHeader.js

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -7,7 +7,7 @@ export default function SiteHeader() {
     <Link href="/" className="site-brand">K710</Link>
   <nav className="site-nav" aria-label="Main site">
     <Link href="/">Home</Link>
-  <Link href="/player-record">Rallies</Link>
+  <Link href="/player-record">Members</Link>
   <Link href="/interest">Interest</Link>
   <Link href="/admin">Admin</Link>
     </nav>
